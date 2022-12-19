931f42baee95c3ba911d47654a1f7e702b86bed9
Jasper Desnyder <jasper.desnyder@student.howest.be>
2022-12-19 19:13:11 +0100

    Added listview with necessary styling and javascript code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,6 @@
 
 /* TODO's:
       [] Add event listeners to new forms
-      [] Set up express server
       [] Add sorting possibilities to notes when retrieving them (Add new endpoint for this?)
       [] Add layout for list view
       [] Add possibility to favourite a note
@@ -14,6 +13,7 @@ const BASEURL = "http://localhost:3000";
 document.addEventListener("DOMContentLoaded", init);
 
 async function init() {
+    // TODO: Fix search bar being a bitch
     document.querySelector("#input_search").addEventListener("focus", () => {
         document.querySelector("label").classList.add("active");
     });
@@ -26,7 +26,36 @@ async function init() {
 
     document.querySelector("dialog #cancel").addEventListener("click", closeDialogWithoutClearingText);
 
-    // await addNotes();
+    document.querySelector("#listView").addEventListener("click", showNotesInListView);
+    document.querySelector("#cardView").addEventListener("click", showNotesInCardView);
+
+    await addNotesInCardView();
+}
+
+async function showNotesInListView() {
+    document.querySelector("#listView").classList.add("selected");
+    document.querySelector("#cardView").classList.remove("selected");
+
+    const $notesDiv = document.querySelector("#cards");
+
+    $notesDiv.classList.remove("cardView");
+    $notesDiv.classList.add("listView");
+    $notesDiv.innerHTML = "";
+
+    await addNotesInListView();
+}
+
+async function showNotesInCardView() {
+    document.querySelector("#cardView").classList.add("selected");
+    document.querySelector("#listView").classList.remove("selected");
+
+    const $notesDiv = document.querySelector("#cards");
+
+    $notesDiv.classList.add("cardView");
+    $notesDiv.classList.remove("listView");
+    $notesDiv.innerHTML = "";
+
+    await addNotesInCardView();
 }
 
 function createNewNote() {
@@ -39,18 +68,57 @@ function closeDialogWithoutClearingText() {
     $dialog.close();
 }
 
-async function addNotes() {
+async function addNotesInListView() {
+    const $notesDiv = document.querySelector("#cards");
+    $notesDiv.innerHTML =  `<div id=notesList></div>
+                            <div id=fullNote></div>`;
+
+    const $notesListDiv = document.querySelector("#notesList");
+
+    const notes = await fetch(`${BASEURL}/notes`).then(res => res.json());
+    notes.forEach(note => {
+        const html = `<div class="note">
+                         <h2>${note.title}</h2>
+                         <p>${note.content}</p>
+                         <p class="date">${note.date.toString().split("T")[0]}</p>
+                      </div>`;
+
+        $notesListDiv.insertAdjacentHTML("beforeend", html);
+    });
+
+    addEventListenersToNotes();
+}
+
+function addEventListenersToNotes() {
+    document.querySelectorAll("#notesList .note").forEach(note => {
+        note.addEventListener("click", showNote);
+    });
+}
+
+function showNote(e) {
+    const $fullNoteDiv = document.querySelector("#fullNote");
+
+    const note = e.target.closest(".note");
+    const title = note.children[0].innerText;
+    const content = note.children[1].innerText;
+    const date = note.children[2].innerText;
+
+    $fullNoteDiv.innerHTML = `<h2>${title}</h2>
+                              <p>${content}</p>
+                              <p class="date">${date}</p>`;
+}
+
+async function addNotesInCardView() {
     const $notesDiv = document.querySelector("#cards");
 
     const notes = await fetch(`${BASEURL}/notes`).then(res => res.json());
     notes.forEach(note => {
-        // TODO: Use templates?
         const card = `<div class="card">
                           <h2>${note.title}</h2>
                           <p>${note.content}</p>
-                          <p class="date">${note.date.getDay()}/${note.date.getMonth()}/${note.date.getFullYear()}</p>
+                          <p class="date">${note.date.split("T")[0]}</p>
                       </div>`;
 
         $notesDiv.insertAdjacentHTML("beforeend", card);
-    })
+    });
 }
