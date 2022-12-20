d33b8e4269bf8706a4f26feaf65fa20d58867a0b
Jasper Desnyder <jasper.desnyder@student.howest.be>
2022-12-20 20:47:58 +0100

    Added small notification when adding/removing note

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,11 @@
 
 document.addEventListener("DOMContentLoaded", init);
 
+let notificationPermission;
+
 async function init() {
+    notificationPermission = await Notification.requestPermission();
+
     const $inputSearch = document.querySelector("#input_search");
     $inputSearch.addEventListener("focus", () => {
         document.querySelector("label.search").classList.add("active");
@@ -203,17 +207,24 @@ async function addNoteToFavourites(e) {
     const id = note.dataset.id;
 
     let notes;
+    let notif;
 
     if (e.target.classList.contains("fa-solid")) {
         notes = await remove(`/notes/favourites/${id}`)
                         .then(res => res.json())
                         .then(data => { return data; });
+
+        notif = new Notification("Note removed from favourites!");
     }
     else {
         notes = await post(`/notes/favourites/${id}`)
                         .then(res => res.json())
                         .then(data => { return data; });
+
+        notif = new Notification("Note added to favourites!");
     }
+
+    setTimeout(() => notif.close(), 1500);
 
     await showNotes(notes);
 }
